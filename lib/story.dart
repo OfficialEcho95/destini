f325f2cf0f1239fcc0e4a2da5bb06172e6b7da6c
Emmanuel chukwu <Officialecho95@outlook.com>
2022-09-13 04:36:30 +0100

    All the functioning of the app are done here

---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+class Story{
+
+  late String title, a, b;
+
+  Story({required storyTitle,required String choice1, required String choice2,}){
+    title = storyTitle;
+    a = choice1;
+    b = choice2;
+  }
+}
